b69b8e71f9a871f757c8f5e601a6194502b105e7
EvanBG42 <evanbg42@gmail.com>
2018-11-28 15:17:08 -0500

    adding online output

---
+++ Output/Evan/DC-Online/Data/SearchUrl_Chunk0.js
@@ -0,0 +1,1 @@
+define({"../Content/HStreet.htm":7,"../Content/Home.htm":8,"../Content/Introduction.htm":3,"../Content/Metro.htm":4,"../Content/Monuments.htm":6,"../Content/Museums.htm":5,"../Content/PrintOnlyTopics/Copyright.htm":1,"../Content/PrintOnlyTopics/PrintTOC.htm":2,"../Content/PrintOnlyTopics/TitlePage.htm":0,"../Content/WebOnlyTopics/Resources.htm":9,});
---
+++ Output/Evan/DC-Online/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=636790141407303097'
+});
